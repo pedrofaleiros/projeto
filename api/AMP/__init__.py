1f591374d9764a7b45a2c753c05e48595aba2c0f
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-05-29 12:33:18 -0300

    commit

--- api/AMP/__init__.py
+++ api/AMP/__init__.py
@@ -5,6 +5,71 @@ from AMP.modelos.professor import ProfessorCadastro
 from fastapi.middleware.cors import CORSMiddleware
 
 #conectar banco de dados e fazer get e post
+def quicksort(pessoas, inicio, final):
+
+    if inicio >= final:
+        return
+
+    i = inicio
+    pivo = pessoas[i]['preco']
+    j = final
+
+    while j != i:
+        if i > j:
+            if pessoas[j]['preco'] > pivo:
+                aux = pessoas[i]['nome']
+                pessoas[i]['nome'] = pessoas[j]['nome']
+                pessoas[j]['nome'] = aux
+
+                aux = pessoas[i]['preco']
+                pessoas[i]['preco'] = pessoas[j]['preco']
+                pessoas[j]['preco'] = aux
+
+                aux = pessoas[i]['materia']
+                pessoas[i]['materia'] = pessoas[j]['materia']
+                pessoas[j]['materia'] = aux
+
+                aux = pessoas[i]['contato']
+                pessoas[i]['contato'] = pessoas[j]['contato']
+                pessoas[j]['contato'] = aux
+                
+                aux = pessoas[i]['obs']
+                pessoas[i]['obs'] = pessoas[j]['obs']
+                pessoas[j]['obs'] = aux
+                
+                i, j = j, i
+
+        else:
+            if pessoas[j]['preco'] < pivo:
+                aux = pessoas[i]['nome']
+                pessoas[i]['nome'] = pessoas[j]['nome']
+                pessoas[j]['nome'] = aux
+
+                aux = pessoas[i]['preco']
+                pessoas[i]['preco'] = pessoas[j]['preco']
+                pessoas[j]['preco'] = aux
+
+                aux = pessoas[i]['materia']
+                pessoas[i]['materia'] = pessoas[j]['materia']
+                pessoas[j]['materia'] = aux
+
+                aux = pessoas[i]['contato']
+                pessoas[i]['contato'] = pessoas[j]['contato']
+                pessoas[j]['contato'] = aux
+                
+                aux = pessoas[i]['obs']
+                pessoas[i]['obs'] = pessoas[j]['obs']
+                pessoas[j]['obs'] = aux
+
+                i, j = j, i
+
+        if i > j:
+            j += 1
+        else:
+            j -= 1
+    
+    quicksort(pessoas, inicio, i-1)
+    quicksort(pessoas, i+1, final)
 
 def abre_conexao(nome_db):
     conexao = pymysql.connect(
@@ -42,6 +107,8 @@ async def recupera_prof():
     #algoritmo aqui
     #passava tempo de execucao tbm
     fecha_conexao(conexao, cursor)
+
+    quicksort(dados, 0, len(dados)-1)
 
     return {"resposta":dados}
 
